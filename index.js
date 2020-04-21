595a66b94b91e90f42ab1f47fea128a096f36e92
leonardo pernett <pernettleonardo@gmail.com>
2020-04-20 22:01:56 -0500

    my first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-main-left').classList.toggle('show')
+})
+
+ScrollReveal().reveal('.showcase',{delay:1000});
+ScrollReveal().reveal('.card-body',{delay:1000});
+ScrollReveal().reveal('.card-banner-one',{delay:1000});
+ScrollReveal().reveal('.card-banner-two',{delay:1000});
+ScrollReveal().reveal('.footer-links',{delay:1000});
